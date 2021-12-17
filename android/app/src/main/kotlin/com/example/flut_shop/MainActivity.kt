38f019a6f7b902bef1e03ca3d729af294daa0776
ignac_borovy <i.borovy@gmail.com>
2021-12-17 20:13:39 +0100

    Make basic structure

---
+++ android/app/src/main/kotlin/com/example/flut_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flut_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/presentation/my_app.dart
@@ -0,0 +1,15 @@
+import 'package:flut_shop/src/core/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: kAppTitle,
+      home: Container(),
+    );
+  }
+}
